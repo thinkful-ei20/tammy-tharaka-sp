f68a35bb57f76005e482b2881608a7d2f14ae462
Tammy Tian <tiantammy@outlook.com>
2018-07-09 22:48:30 +0200

    fixed typo

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 require('dotenv').config();
 export const API_BASE_URL =
-    process.env.REACT_APP_API_BASE_URL || 'https://tammy-tharaka-sr-server-1.herokuapp.com';
+    process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
