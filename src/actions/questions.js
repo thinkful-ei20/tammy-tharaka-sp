c89be7eb1af9e9765eb9a81c26a61e75e3af7061
Tharaka Wijekularatne <tharusnow@gmail.com>
2018-07-11 05:53:51 -0700

    remove comments

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -3,37 +3,6 @@ import {SubmissionError} from 'redux-form';
 import {API_BASE_URL} from '../config';
 import {normalizeResponseErrors} from './utils';
 
-// export const SET_QUESTIONS = 'SET_QUESTIONS';
-// export const setQuestions = userQuestions => ({
-//     type: SET_QUESTIONS,
-//     userQuestions
-// });
-
-// //GET USER QUESTIONS
-// export const getQuestions = (event) => (dispatch, getState) => {
-//     const authToken = getState().auth.authToken;
-//     return fetch(`${API_BASE_URL}/questions`, {
-//         method: 'GET',
-//         headers: {
-//             Authorization: `Bearer ${authToken}`,
-//             'content-type': 'application/json'
-//         },
-//     })
-//         .then(res => normalizeResponseErrors(res))
-//         .then(res => res.json())
-//         .then(qauestions => dispatch(setQuestions(qauestions)))
-//         .catch(err => {
-//             const {reason, message, location} = err;
-//             if (reason === 'ValidationError') {
-//                 return Promise.reject(
-//                     new SubmissionError({
-//                         [location]: message
-//                     })
-//                 );
-//             }
-//         });
-// };
-
 
 export const FETCH_QUESTIONS_SUCCESS = 'FETCH_QUESTIONS_SUCCESS';
 export const fetchQuestionsSuccess = data => ({
